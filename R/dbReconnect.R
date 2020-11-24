3d3415c3d2d01b9f79a94f4af0e98087af8b5cdf
Jake Tolbert <jaketolbert@berkeley.edu>
2020-11-24 08:52:09 -0600

    Change dbReconnect() to take a database connection as an argument, rather than acting like magic.

--- R/dbReconnect.R
+++ R/dbReconnect.R
@@ -1,19 +1,19 @@
 
 is_db_tbl  <- function(tblname) {
-  
-  get(tblname) %>% 
-    class() %>% 
-    str_detect("tbl_sql") %>% 
+
+  get(tblname) %>%
+    class() %>%
+    str_detect("tbl_sql") %>%
     any()
 }
 
 
 dbMakeTableObj  <- function(tblname, con = cdw) {
-  
+
   x  <- tbl(con, in_schema("CDW", str_to_upper(tblname)) )
-  
+
   assign(tblname, x, env = rlang::global_env())
-  
+
 }
 
 
@@ -21,54 +21,71 @@ dbMakeTableObj  <- function(tblname, con = cdw) {
 #   but is dependent on figuring out how to connect tbl_sql with their parent connection
 
 #' dbReconnect
-#' 
+#'
 #' @description reconnect to a database and reinitialize all the tables
-#' 
+#'
+#' @param con a existing database connection to reconnect to
 #' @details basically just works with cdw connections at this point
 #'
-#' @return returns TRUE invisibly 
-#' 
+#' @return returns TRUE invisibly
+#'
 #' @export
-dbReconnect  <- function() {
-  
-  all_objs_str  <- ls(envir = rlang::global_env())
-  
-  all_objs_class  <- map(all_objs_str, ~get(.x) %>% class)
-  
-  tablesi  <- map_lgl(all_objs_class, ~ any(str_detect(.x, "tbl_sql")) )
-  dbtables <- all_objs_str[tablesi]
-  
-  connectionsi <- map_lgl(all_objs_class, ~ any(str_detect(.x, "^Oracle$")))
-  dbconnections  <- all_objs_str[connectionsi]
-  
-  if(length(dbconnections == 1) & dbconnections[[1]] == 'cdw') {
-    cdwonly  <- T
-    wipeall  <- T
-  } else {
-    cdwonly  <- F
-    wipeall  <- rstudioapi::showQuestion("You have more than just a CDW connection active. Are you sure you want to wipe out all database connetions?")
+dbReconnect  <- function(con) {
+
+
+  # all_objs_str  <- ls(envir = rlang::global_env())
+  #
+  # all_objs_class  <- map(all_objs_str, ~get(.x) %>% class)
+  #
+  # tablesi  <- map_lgl(all_objs_class, ~ any(str_detect(.x, "tbl_sql")) )
+  # dbtables <- all_objs_str[tablesi]
+  #
+  # connectionsi <- map_lgl(all_objs_class, ~ any(str_detect(.x, "^Oracle$")))
+  # dbconnections  <- all_objs_str[connectionsi]
+  #
+  # if(length(dbconnections == 1) & dbconnections[[1]] == 'cdw') {
+  #   cdwonly  <- T
+  #   wipeall  <- T
+  # } else {
+  #   cdwonly  <- F
+  #   wipeall  <- rstudioapi::showQuestion("You have more than just a CDW connection active. Are you sure you want to wipe out all database connetions?")
+  # }
+  #
+  #
+  # if(cdwonly | wipeall) {
+  #
+  #   map(dbconnections, ~DBI::dbDisconnect(get(.x)))
+  #
+  # }
+  #
+  # if(cdwonly) {
+  #
+  #   cdw <- DBI::dbConnect(odbc::odbc(), "CDW2", uid ='jaketolbert', pwd = Sys.getenv('CDW_PWD'), timeout = 10)
+  #   assign("cdw", cdw, env = rlang::global_env())
+  #
+  #   walk(dbtables, dbMakeTableObj )
+  # }
+
+  con_name  <- deparse(substitute(con))
+
+  if(!exists(con_name)) {
+    stop("Please enter an existing database connection.")
   }
-  
-  
-  if(cdwonly | wipeall) {
-    
-    map(dbconnections, ~DBI::dbDisconnect(get(.x)))
-    
-  }
-  
-  if(cdwonly) {
-    
-    cdw <- DBI::dbConnect(odbc::odbc(), "CDW2", uid ='jaketolbert', pwd = Sys.getenv('CDW_PWD'), timeout = 10)
-    assign("cdw", cdw, env = rlang::global_env())
-    
-    walk(dbtables, dbMakeTableObj )
-  }
-  
-  
+
+
+  con_source  <- con@info$sourcename
+
+  DBI::dbDisconnect(con)
+
+  newcon  <- DBI::dbConnect(odbc::odbc(), con_source, uid = Sys.getenv("CDW_USR"), pwd = Sys.getenv("CDW_PWD"), timeout = 10)
+  assign(con_name, newcon, env = rlang::global_env())
+
+
+
   # need to figure out how to match tables up with db connections when you hve more than one connection
-  
+
   invisible(TRUE)
-  
+
 }
 
 
