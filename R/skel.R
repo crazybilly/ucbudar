c0605965a3ef944f094003808c6bb4d1f9c36bb0
Jake Tolbert <jaketolbert@berkeley.edu>
2021-04-15 13:53:32 -0500

    Add /sql directory as part of Rproject skeleton in skel()

--- R/skel.R
+++ R/skel.R
@@ -1,6 +1,6 @@
 
 #' Create Rproject Skeleton
-#' 
+#'
 #' @description Creates the folder structure and optional basescript for an R project. Creates two directories, data and output, assuming they don't already exist as well as the base script.
 #'
 #' @param createbasescript a logical value indicating whether or not a base script should be created. The base script will source all R scripts in the working directory
@@ -10,42 +10,45 @@
 #' @export
 #'
 skel  <- function( createbasescript = F, basescriptname = '00-source-all-files.R', path = NULL) {
-  
+
   if(is.null(path) ) {
     path  <- getwd()
   }
-  
-  
+
+
   if( !dir.exists('output') ) {
     dir.create(paste0(path,'/output'))
   }
-  
+
   if( !dir.exists('data') ) {
     dir.create(paste0(path,'/data'))
   }
-  
-  
+
+
+  if( !dir.exists('sql') ) {
+    dir.create(paste0(path,'/sql'))
+  }
+
   if( createbasescript ) {
-    
+
     allRscripts   <- dir(pattern = '\\.R$')
     allRscripts  <- allRscripts[allRscripts != basescriptname]
-    
+
     scripttext  <- paste(
       sub("(.*)", "source('\\1', echo = T)", allRscripts)
       , collapse = "\n"
     )
-    
+
     if( file.exists(basescriptname) ) {
       prompt <- readline(paste0("Do you want to overwrite ",basescriptname,"? [Y]"))
     } else {
       prompt  <- NA
     }
-    
+
     if( is.blank(prompt) | grepl("y",prompt, ignore.case = T) ) {
       writeLines(paste0(scripttext,"\n\nbeepr::beep()"), basescriptname)
     }
-    
+
   }
-  
+
 }
-    
